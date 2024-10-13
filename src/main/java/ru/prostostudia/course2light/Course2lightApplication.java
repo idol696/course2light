7f400661738beb3332dadf25acf3714f9b380feb
Roman Kuzmin <ghelatin@mail.ru>
2024-10-13 11:24:48 +0300

    commit init

---
+++ src/main/java/ru/prostostudia/course2light/Course2lightApplication.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.course2light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2lightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2lightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/prostostudia/course2light/Course2lightApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.course2light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2lightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
